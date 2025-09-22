95b99ab8c2d6579d0c8b2b2ff7db6b7f9998c4d5
guustavomc <gustavomccb@gmail.com>
2025-09-22 09:23:39 -0300

    pir sensor example

--- examples/sensor-PIR-motion/sensor-PIR-motion.ino
+++ examples/sensor-PIR-motion/sensor-PIR-motion.ino
@@ -1,5 +1,4 @@
 //Code from https://randomnerdtutorials.com/arduino-with-pir-motion-sensor/
-
 /*  
     Arduino with PIR motion sensor
     For complete project details, visit: http://RandomNerdTutorials.com/pirsensor
@@ -8,7 +7,6 @@
  
 int led = 13;                // the pin that the LED is atteched to
 int sensor = 2;              // the pin that the sensor is atteched to
-int state = LOW;             // by default, no motion detected
 int val = 0;                 // variable to store the sensor status (value)
 
 void setup() {
@@ -22,19 +20,13 @@ void loop(){
   if (val == HIGH) {           // check if the sensor is HIGH
     digitalWrite(led, HIGH);   // turn LED ON
     delay(100);                // delay 100 milliseconds 
+    Serial.println("Motion detected!"); 
     
-    if (state == LOW) {
-      Serial.println("Motion detected!"); 
-      state = HIGH;       // update variable state to HIGH
-    }
   } 
   else {
-      digitalWrite(led, LOW); // turn LED OFF
-      delay(200);             // delay 200 milliseconds 
-      
-      if (state == HIGH){
-        Serial.println("Motion stopped!");
-        state = LOW;       // update variable state to LOW
-    }
+    digitalWrite(led, LOW); // turn LED OFF
+    delay(200);             // delay 200 milliseconds 
+    Serial.println("Motion stopped!");
+    
   }
 }
